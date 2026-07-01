dc723bee9145d87ff38c3f17c430aedfa0cfbd1f
gr33nimax <noname.play@yandex.ru>
2026-07-01 23:49:02 +0300

    Remove tls directive completely to allow default automatic ACME TLS config in Caddy

--- tests/test_naive_plugin.py
+++ tests/test_naive_plugin.py
@@ -178,7 +178,7 @@ def test_build_caddyfile_basic():
     assert "probe_resistance mysecret123" in caddyfile
     assert "forward_proxy" in caddyfile
     assert "reverse_proxy https://www.google.com" in caddyfile
-    assert "tls" in caddyfile
+    assert "tls" not in caddyfile
 
 
 def test_build_caddyfile_no_probe_secret():
