a80a2f02e971de9ccfbb5fb9aa14f642d4da7862
gr33nimax <noname.play@yandex.ru>
2026-07-17 22:37:53 +0300

    docs: rewrite project README

--- bootstrap.sh
+++ bootstrap.sh
@@ -140,7 +140,7 @@ fi
 echo -e "\n${BOLD}[4/5] Загрузка HYDRA${NC}"
 INSTALL_DIR="/opt/hydra"
 REPO_URL="https://github.com/gr33nimax/HYDRA-ULTIMATE"
-BRANCH="dev"
+BRANCH="main"
 
 if [[ -d "${INSTALL_DIR}/.git" ]]; then
     info "Обновление репозитория..."
